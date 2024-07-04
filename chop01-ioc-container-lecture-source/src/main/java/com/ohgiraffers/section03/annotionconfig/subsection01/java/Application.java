7028b1514f3d92fad8f38a8db9f52addfc6aa780
kim <modan8199@gmail.com>
2024-07-04 16:13:58 +0900

    commit

---
+++ chop01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section03/annotionconfig/subsection01/java/Application.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section03.annotionconfig.subsection01.java;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
+
+        /*
+        getBeanDefinitionNames : 스프링 컨테이너에서 생성된 bean 들의 이름을 배열로 반환한다.
+        * * */
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        for (String beanName : beanNames){
+            System.out.println("beanName = " + beanName);
+        }
+    }
+}
